ac950efa52a6811126df890ba03aaa760452e2ef
Hossain Muctadir <h.m.muctadir@tue.nl>
2022-03-02 12:50:05 +0100

    create theme backend

--- webapp/src/database/queries/theme_queries.py
+++ webapp/src/database/queries/theme_queries.py
@@ -1,8 +1,29 @@
-from sqlalchemy import select
+from typing import List
+
+from sqlalchemy import select, insert
 
 from src import db
-from src.database.models import Theme
+from src.database.models import Theme, LabelingData
 
 
-def get_all_themes():
+def get_all_themes() -> List[Theme]:
     return [t for t, in db.session.execute(select(Theme)).all()]
+
+
+def get_theme_by_id(tid: int) -> Theme:
+    return db.session.execute(select(Theme).where(Theme.id == tid)).scalar()
+
+
+def get_theme_by_name(name: str) -> Theme:
+    return db.session.execute(select(Theme).where(Theme.theme == name)).scalar()
+
+
+def create_theme(name: str, description: str, label_ids: List[int], creator: str) -> Theme:
+    if get_theme_by_name(name) is not None:
+        raise ValueError('Theme with this name exists')
+
+    labels = db.session.execute(select(LabelingData).where(LabelingData.id in label_ids)).all()
+    theme = Theme(theme=name, theme_description=description, labels=labels, created_by=creator)
+    db.session.add(theme)
+    db.session.commit()
+    return theme
--- webapp/src/routes/routes_theme.py
+++ webapp/src/routes/routes_theme.py
@@ -1,9 +1,12 @@
-from flask import render_template
+from typing import List
+
+from flask import render_template, request, jsonify
 from flask_login import login_required
 
 from src import app
 from src.database.queries.label_queries import get_all_labels
-from src.database.queries.theme_queries import get_all_themes
+from src.database.queries.theme_queries import get_all_themes, create_theme
+from src.helper.tools_common import string_none_or_empty, who_is_signed_in
 
 
 @app.route('/theme_management', methods=['GET'])
@@ -12,11 +15,29 @@ def theme_management_view():
     return render_template('theme_pages/theme_management.html', themes=get_all_themes())
 
 
-# @app.route('/theme_management/create', methods=['GET'])
-# @login_required
-# def theme_create_view():
-#     all_labels = get_all_labels()
-#     return render_template('theme_pages/theme_create.html', all_labels=all_labels)
+@app.route('/theme_management/create_theme', methods=['GET'])
+@login_required
+def theme_create_view():
+    all_labels = get_all_labels()
+    return render_template('theme_pages/theme_create.html', all_labels=all_labels)
+
+
+@app.route('/theme_management/create_theme', methods=['POST'])
+@login_required
+def theme_create_post():
+    name = request.form.get('theme', '', str)
+    description = request.form.get('description', '', str)
+    label_ids = request.form.get('label_ids[]', [], List[int])
+
+    if string_none_or_empty(name) or string_none_or_empty(description) or len(label_ids) == 0:
+        return jsonify({'status': 'Empty arguments'}), 400
+
+    try:
+        create_theme(name.strip(), description.strip(), label_ids, who_is_signed_in())
+    except (ValueError, Exception) as e:
+        return jsonify({'status': str(e)}), 400
+
+    return jsonify({'status': 'theme created'})
 
 
 @app.route('/theme_management/delete_theme/<theme_id>', methods=['DELETE'])
